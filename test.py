c89135888ee434d84ed6f00f5c4db8f3661c33cc
Esteban Jimenez <esteban.gab.jimenez@gmail.com>
2024-04-26 18:55:25 +1000

    modifies test

--- test.py
+++ test.py
@@ -1,20 +1,14 @@
-from scipy import sparse
-
-columns = 500
-rows = 500
-
-def precreated_list():
-
-    row, col, val = [], [], []
-
-    for i in range(rows):
-        for j in range(columns):
-            row.append(i)
-            col.append(j)
-            val.append(1)
-
-    breakpoint()
-    sparse.csc_matrix((val, (row, col)))
+from src.pyspde import anisotropy, Grid, Spde, anisotropy_from_svg
+import matplotlib.pyplot as plt
 
 
-precreated_list()
+
+atpy = anisotropy_from_svg(r'assets/anticline.svg', beta=50, gamma=0.5)
+grid = Grid(nx=400, ny=200, anisotropy=atpy)
+
+sp = Spde(grid, sigma=1, a=50)
+Z = sp.simulate(seed=0)
+plt.imshow(Z)
+plt.tight_layout()
+
+plt.savefig('anticline_simu.png', dpi=600, bbox_inches='tight')
